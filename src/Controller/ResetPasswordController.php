bc27a87c1a292a4d911479bfd096f68763b27c89
Jawher Ayari <159702884+jawh33r@users.noreply.github.com>
2025-11-17 16:16:23 +0100

    forget password controller and entity

---
+++ src/Controller/ResetPasswordController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+final class ResetPasswordController extends AbstractController
+{
+    #[Route('/forgot-password', name: 'reset_password')]
+    public function index(): Response
+    {
+        return $this->render('reset_password/index.html.twig', [
+            'controller_name' => 'ResetPasswordController',
+        ]);
+    }
+}
